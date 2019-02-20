e2999b690c726636c25d0d26350da397da0380ca
nelson <1051988660@qq.com>
2019-02-20 11:05:21 +0800

    Create datastore.js

---
+++ src/renderer/store/datastore.js
@@ -0,0 +1,80 @@
+import db from 'electron-db'
+import { remote } from 'electron'
+function delay () {
+  return new Promise(resolve => setTimeout(resolve, 100))
+}
+class DB {
+  constructor (app) {
+    console.log('init db', app)
+  }
+  static judgeFolder () {
+    return new Promise((resolve, reject) => {
+      const fs = remote.require('fs')
+      fs.stat('data', (err, stats) => {
+        if (err || !stats) {
+          fs.mkdir('data')
+          resolve()
+        } else {
+          resolve()
+        }
+      })
+    })
+  }
+  createTable () {
+    db.createTable('contents', (success, msg) => {
+      if (success) {
+        console.log('create success')
+      } else {
+        console.log('create failed')
+      }
+      console.log('msg:', msg)
+    })
+  }
+  insertTable (obj) {
+    return new Promise((resolve, reject) => {
+      db.insertTableContent('contents', obj, (success, msg) => {
+        resolve(success)
+      })
+    })
+  }
+  async getAll () {
+    await delay()
+    return new Promise((resolve, reject) => {
+      db.getAll('contents', (succ, data) => {
+        if (succ) {
+          resolve(data)
+        }
+        reject(data)
+      })
+    })
+  }
+  deleteContent (c) {
+    return new Promise((resolve, reject) => {
+      db.deleteRow('contents', {'id': c.id}, (succ, msg) => {
+        resolve(succ)
+      })
+    })
+  }
+  updateContent (c) {
+    let where = {
+      id: c.id
+    }
+    return new Promise((resolve, reject) => {
+      db.updateRow('contents', where, c, (succ, msg) => {
+        resolve(succ)
+      })
+    })
+  }
+  async searchContent (c) {
+    await delay()
+    return new Promise((resolve, reject) => {
+      db.search('contents', 'title', c, (succ, data) => {
+        if (succ) {
+          resolve(data)
+        }
+        resolve(null)
+      })
+    })
+  }
+}
+export default DB
