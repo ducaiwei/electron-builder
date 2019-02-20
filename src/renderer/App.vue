ba8f3db1f241345dc7ae20164d9a54a4694d2668
nelson <1051988660@qq.com>
2019-02-20 11:05:50 +0800

    Create App.vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'notes',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html,body {
+  height: 100%;
+}
+ul, li {
+  list-style: none;
+}
+input, select, button, input:focus{
+  border: none;
+  -webkit-appearance: none;
+}
+button {
+  outline: none
+}
+#app {
+  height: 100%;
+  overflow: auto;
+}
+</style>
