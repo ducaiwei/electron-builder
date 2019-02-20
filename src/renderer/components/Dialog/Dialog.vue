67e2a9c831619436d43ec3a6c9ec6ae07fdf2031
nelson <1051988660@qq.com>
2019-02-20 10:39:48 +0800

    Create Dialog.vue

---
+++ src/renderer/components/Dialog/Dialog.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="dialog-box">
+    <div class="mask"></div>
+</div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss">
+.dialog-box {
+    position: absolute;
+    display: block;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+.mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  min-height: 100%;
+  background-color: rgba(0,0,0,.6);
+  z-index: 100;
+}
+</style>
+
